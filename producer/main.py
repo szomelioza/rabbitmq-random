c073ea8c843940ecdc213576653dec617b873ced
szomelioza <kamil.szpilak@outlook.com>
2025-03-09 14:43:25 +0100

    Producer refactor

--- producer/main.py
+++ producer/main.py
@@ -1,20 +1,50 @@
 import os
+import time
+from random import randint
+from uuid import uuid4
 
 import pika
 
 RABBIT_MQ_HOST = os.getenv("RABBIT_MQ_HOST")
 QUEUE_NAME = os.getenv("QUEUE_NAME")
+SENT_MSG_LIMIT = int(os.getenv("SENT_MSG_LIMIT", "100"))
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(RABBIT_MQ_HOST)
-)
-channel = connection.channel()
 
-channel.queue_declare(queue=QUEUE_NAME)
+def get_connection():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(RABBIT_MQ_HOST)
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE_NAME)
+    return connection, channel
 
-msg = "Test message"
 
-channel.basic_publish(exchange="", routing_key=QUEUE_NAME, body=msg)
-print(f" [x] Sent {msg}")
+def send_message(channel):
+    msg = uuid4().hex
+    channel.basic_publish(exchange="", routing_key=QUEUE_NAME, body=msg)
+    print(f" Sent {msg}")
 
-connection.close()
+
+def random_sleep():
+    time.sleep(randint(1, 10))
+
+
+def main():
+    print("Producer starts...")
+    connection, channel = get_connection()
+
+    try:
+        for _ in range(SENT_MSG_LIMIT):
+            send_message(channel)
+            random_sleep()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        connection.close()
+        print("Producer ends.")
+
+
+if __name__ == "__main__":
+    main()
