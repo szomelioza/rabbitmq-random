dbde8cdb39bc5b9e94aceb0a04ac3befba1e48a1
szomelioza <kamil.szpilak@outlook.com>
2025-03-09 14:51:53 +0100

    Consumer refactor

--- consumer/main.py
+++ consumer/main.py
@@ -1,19 +1,50 @@
 import os
+import time
+from random import randint
 
 import pika
 
 RABBIT_MQ_HOST = os.getenv("RABBIT_MQ_HOST")
 QUEUE_NAME = os.getenv("QUEUE_NAME")
 
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(RABBIT_MQ_HOST)
-)
-channel = connection.channel()
 
-_, _, body = channel.basic_get(queue=QUEUE_NAME, auto_ack=True)
+def get_connection():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(RABBIT_MQ_HOST)
+    )
+    channel = connection.channel()
+    channel.queue_declare(queue=QUEUE_NAME)
+    return connection, channel
 
-if body:
-    print(f" [x] Received {body}")
-else:
-    print("Queue empty!")
-connection.close()
+
+def random_sleep():
+    time.sleep(randint(1, 10))
+
+
+def callback(ch, method, properties, body):
+    print(f" Received {body.decode()}")
+    random_sleep()
+
+
+def main():
+    print("Consumer starts...")
+    connection, channel = get_connection()
+
+    try:
+        channel.basic_consume(
+            queue=QUEUE_NAME,
+            on_message_callback=callback,
+            auto_ack=True
+        )
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        connection.close()
+        print("Consumer ends.")
+
+
+if __name__ == "__main__":
+    main()
