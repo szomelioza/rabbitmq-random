fa9f8daa0ccfee4912e9be58d39a7c4eefef9a05
szomelioza <kamil.szpilak@outlook.com>
2025-03-13 22:16:23 +0100

    Consumer refactor

--- consumer/main.py
+++ consumer/main.py
@@ -12,6 +12,80 @@ from consumer.constants import CONFIG_VALUES
 logger = get_logger()
 
 
+def load_config() -> Config:
+    """Load config from environment variables
+
+    Returns:
+        Config: Object that contains parsed env vars
+    """
+    try:
+        config = Config(CONFIG_VALUES).read()
+        return config
+    except RequiredEnvVarNotSet as e:
+        logger.error(f"Required env var not set: {e}")
+        sys.exit(1)
+    except InalidEnvVar as e:
+        logger.error(f"Invalid value for variable: {e}")
+        sys.exit(1)
+
+
+def connect(host: str, queue: str) -> tuple[
+    pika.BlockingConnection,
+    pika.channel.Channel
+]:
+    """Establish connection with RabbitMQ host
+
+    Args:
+        host (str): Address of RabbitMQ server
+        queue (str): Name of the queue to use
+
+    Returns:
+        tuple: RabbitMQ connection (pika.BlockingConnection) and
+        RabbitMQ channel (pika.channel.Channel) for receiving messages
+    """
+    try:
+        connection, channel = get_connection(
+            host,
+            queue
+        )
+        return connection, channel
+    except Exception as e:
+        logger.error(e)
+        sys.exit(1)
+
+
+def run_loop(
+    channel: pika.channel.Channel,
+    queue_name: str,
+    min_sleep: int,
+    max_sleep: int
+):
+    """Run program loop to listen and print messages
+    from the queue
+
+    Args:
+        channel (pika.channel.Channel): Channel used to receive messages
+        queue_name (str): Name of the queue
+        min_sleep (int): Minimum time to sleep
+        max_sleep (int): Maximum time to sleep
+    """
+    try:
+        channel.basic_consume(
+            queue=queue_name,
+            on_message_callback=functools.partial(
+                callback,
+                min_sleep=min_sleep,
+                max_sleep=max_sleep
+            ),
+            auto_ack=False
+        )
+        channel.start_consuming()
+    except KeyboardInterrupt:
+        logger.error("KeyboardInterrupt")
+    except Exception as e:
+        logger.error(e)
+
+
 def callback(
     channel: pika.channel.Channel,
     method: pika.spec.Basic.Deliver,
@@ -45,42 +119,22 @@ def main() -> None:
         None
     """
     logger.info("Consumer starts...")
-    try:
-        config = Config(CONFIG_VALUES).read()
-    except RequiredEnvVarNotSet as e:
-        logger.error(f"Required env var not set: {e}")
-        sys.exit(1)
-    except InalidEnvVar as e:
-        logger.error(f"Invalid value for variable: {e}")
-        sys.exit(1)
 
-    try:
-        connection, channel = get_connection(
-            config.RABBITMQ_HOST,
-            config.QUEUE_NAME
-        )
-    except Exception as e:
-        logger.error(e)
-        sys.exit(1)
+    config = load_config()
+    connection, channel = connect(
+        config.RABBITMQ_HOST,
+        config.QUEUE_NAME
+    )
 
-    try:
-        channel.basic_consume(
-            queue=config.QUEUE_NAME,
-            on_message_callback=functools.partial(
-                callback,
-                min_sleep=config.MIN_SLEEP,
-                max_sleep=config.MAX_SLEEP
-            ),
-            auto_ack=False
-        )
-        channel.start_consuming()
-    except KeyboardInterrupt:
-        logger.error("KeyboardInterrupt")
-    except Exception as e:
-        logger.error(e)
-    finally:
-        connection.close()
-        logger.info("Consumer ends.")
+    run_loop(
+        channel=channel,
+        queue_name=config.QUEUE_NAME,
+        min_sleep=config.MIN_SLEEP,
+        max_sleep=config.MAX_SLEEP
+    )
+
+    connection.close()
+    logger.info("Consumer ends.")
 
 
 if __name__ == "__main__":
